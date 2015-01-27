2e1fa82af49c8190a8ebbc88d2bc592e93aa80f9
Jacob LeCoq <jlecoq@eatel.net>
2015-01-27 03:02:09 -0600

    work

---
+++ Assets/Networking Game/Scripts/Testing Scripts/Data.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Data;
+using Mono.Data.SqliteClient;
+public class Data : MonoBehaviour {
+    private string _constr = "URI=file:NPCMaster.db";
+    private IDbConnection _dbc;
+    private IDbCommand _dbcm;
+    private IDataReader _dbr;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
