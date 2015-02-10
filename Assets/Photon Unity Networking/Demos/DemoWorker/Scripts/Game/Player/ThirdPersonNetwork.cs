f30a48a95b71e85e4c41e73d171026724186577e
Jacob LeCoq <jlecoq@eatel.net>
2015-02-10 15:38:08 -0600

    a

--- Assets/Photon Unity Networking/Demos/DemoWorker/Scripts/Game/Player/ThirdPersonNetwork.cs
+++ Assets/Photon Unity Networking/Demos/DemoWorker/Scripts/Game/Player/ThirdPersonNetwork.cs
@@ -16,15 +16,18 @@ public class ThirdPersonNetwork : Photon.MonoBehaviour
         if (stream.isWriting)
         {
             //We own this player: send the others our data
+            stream.SendNext(transform.position);
             stream.SendNext(transform.rotation); 
         }
         else
         {
             //Network player, receive data
+            correctPlayerPos = (Vector3)stream.ReceiveNext();
             correctPlayerRot = (Quaternion)stream.ReceiveNext();
         }
     }
 
+    private Vector3 correctPlayerPos = Vector3.zero; //We lerp towards this
     private Quaternion correctPlayerRot = Quaternion.identity; //We lerp towards this
 
     void Update()
@@ -32,6 +35,7 @@ public class ThirdPersonNetwork : Photon.MonoBehaviour
         if (!photonView.isMine)
         {
             //Update remote player (smooth this, this looks good, at the cost of some accuracy)
+            transform.position = Vector3.Lerp(transform.position, correctPlayerPos, Time.deltaTime * 5);
             transform.rotation = Quaternion.Lerp(transform.rotation, correctPlayerRot, Time.deltaTime * 5);
         }
     }
--- Assets/Third_Person_Network_Turret.cs
+++ Assets/Third_Person_Network_Turret.cs
@@ -16,18 +16,15 @@ public class Third_Person_Network_Turret : Photon.MonoBehaviour {
         if (stream.isWriting)
         {
             //We own this player: send the others our data
-            stream.SendNext(transform.position);
             stream.SendNext(transform.rotation);
         }
         else
         {
             //Network player, receive data
-            correctPlayerPos = (Vector3)stream.ReceiveNext();
             correctPlayerRot = (Quaternion)stream.ReceiveNext();
         }
     }
 
-    private Vector3 correctPlayerPos = Vector3.zero; //We lerp towards this
     private Quaternion correctPlayerRot = Quaternion.identity; //We lerp towards this
 
     void Update()
@@ -35,7 +32,6 @@ public class Third_Person_Network_Turret : Photon.MonoBehaviour {
         if (!photonView.isMine)
         {
             //Update remote player (smooth this, this looks good, at the cost of some accuracy)
-            transform.position = Vector3.Lerp(transform.position, correctPlayerPos, Time.deltaTime * 5);
             transform.rotation = Quaternion.Lerp(transform.rotation, correctPlayerRot, Time.deltaTime * 5);
         }
     }
