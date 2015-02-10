bdf346f2b0b59eaa9d54de60abb42932bdc6dff8
Jacob LeCoq <jlecoq@eatel.net>
2015-02-10 15:29:42 -0600

    on git hub

--- Assets/Third_Person_Network_Turret.cs
+++ Assets/Third_Person_Network_Turret.cs
@@ -1,13 +1,13 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
-public class ThirdPersonNetwork : Photon.MonoBehaviour
-{
-    TankController controllerScript;
+public class Third_Person_Network_Turret : Photon.MonoBehaviour {
+
+    TankGunController controllerScript;
 
     void Awake()
     {
-        controllerScript = GetComponent<TankController>();
+        controllerScript = GetComponent<TankGunController>();
         gameObject.name = gameObject.name + photonView.viewID;
     }
 
@@ -16,15 +16,18 @@ public class ThirdPersonNetwork : Photon.MonoBehaviour
         if (stream.isWriting)
         {
             //We own this player: send the others our data
-            stream.SendNext(transform.rotation); 
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
         }
         else
         {
             //Network player, receive data
+            correctPlayerPos = (Vector3)stream.ReceiveNext();
             correctPlayerRot = (Quaternion)stream.ReceiveNext();
         }
     }
 
+    private Vector3 correctPlayerPos = Vector3.zero; //We lerp towards this
     private Quaternion correctPlayerRot = Quaternion.identity; //We lerp towards this
 
     void Update()
@@ -32,8 +35,8 @@ public class ThirdPersonNetwork : Photon.MonoBehaviour
         if (!photonView.isMine)
         {
             //Update remote player (smooth this, this looks good, at the cost of some accuracy)
+            transform.position = Vector3.Lerp(transform.position, correctPlayerPos, Time.deltaTime * 5);
             transform.rotation = Quaternion.Lerp(transform.rotation, correctPlayerRot, Time.deltaTime * 5);
         }
     }
-
 }
