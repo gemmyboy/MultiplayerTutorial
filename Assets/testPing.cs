79c10d19cce6c9da0516d0125e10f79ba07a8fd0
skyflyer55 <jlecoq@eatel.net>
2015-02-10 13:07:49 -0600

    push it push it.......push it REAL GOOD

---
+++ Assets/testPing.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using Photon;
+public class testPing : PunBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnGUI()
+    {
+        GUILayout.BeginHorizontal();
+        GUILayout.Label(PhotonNetwork.GetPing() + "");
+    }
+}
