f95433d59e683ab2d19477e28044db3108475f79
Jacob LeCoq <jlecoq@eatel.net>
2015-02-10 16:55:27 -0600

    aadddbbb

--- Assets/Networking Game/Scripts/Networking Scripts/Third_Person_Network_Turret.cs
+++ Assets/Networking Game/Scripts/Networking Scripts/Third_Person_Network_Turret.cs
@@ -4,8 +4,13 @@ using System.Collections;
 public class Third_Person_Network_Turret : Photon.MonoBehaviour {
 
     TankGunController controllerScript;
+    public GameObject barrel;
+
     private Vector3 correctPlayerPos = Vector3.zero; //We lerp towards this
     private Quaternion correctPlayerRot = Quaternion.identity; //We lerp towards this
+
+    private Vector3 correctPlayerPosBarrel = Vector3.zero; //We lerp towards this
+    private Quaternion correctPlayerRotBarrel = Quaternion.identity; //We lerp towards this
     void Awake()
     {
         controllerScript = GetComponent<TankGunController>();
@@ -19,12 +24,18 @@ public class Third_Person_Network_Turret : Photon.MonoBehaviour {
             //We own this player: send the others our data
             stream.SendNext(transform.position);
             stream.SendNext(transform.rotation);
+
+            stream.SendNext(barrel.transform.position);
+            stream.SendNext(barrel.transform.rotation);
         }
         else
         {
             //Network player, receive data
             correctPlayerPos = (Vector3)stream.ReceiveNext();
             correctPlayerRot = (Quaternion)stream.ReceiveNext();
+
+            correctPlayerPosBarrel = (Vector3)stream.ReceiveNext();
+            correctPlayerRotBarrel = (Quaternion)stream.ReceiveNext();
         }
     }
 
@@ -35,6 +46,9 @@ public class Third_Person_Network_Turret : Photon.MonoBehaviour {
             //Update remote player (smooth this, this looks good, at the cost of some accuracy)
             transform.position = Vector3.Lerp(transform.position, correctPlayerPos, Time.deltaTime * 5);
             transform.rotation = Quaternion.Lerp(transform.rotation, correctPlayerRot, Time.deltaTime * 5);
+
+            barrel.transform.position = Vector3.Lerp(transform.position, correctPlayerPosBarrel, Time.deltaTime * 5);
+            barrel.transform.rotation = Quaternion.Lerp(transform.rotation, correctPlayerRotBarrel, Time.deltaTime * 5);
         }
     }
 }
