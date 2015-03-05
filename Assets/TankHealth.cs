37673f0a19cf5b5a0605f68032d40b781b00d161
skyflyer55 <jlecoq@eatel.net>
2015-03-05 12:52:22 -0600

    sdf

--- Assets/TankHealth.cs
+++ Assets/TankHealth.cs
@@ -1,29 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TankHealth : MonoBehaviour {
+public class TankHealth : Photon.MonoBehaviour {
 
 	public int health;
+    UIManager uiManager;
 	//public GameObject healthBar;
 
-
+    void Start()
+    {
+        if (!photonView.isMine)
+        {
+            this.enabled = false;
+        }
+        health = 100;
+        uiManager = FindObjectOfType<UIManager>();
+        uiManager.ChangeHealth(health);
+    }
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("CollisionDetected");
         if (other.gameObject.tag == "TankShell")
         {
+            Debug.Log("TankBullet Hit");
             health -= 20;
-            transform.localScale += new Vector3(Mathf.Lerp(transform.localScale.x, transform.localScale.x - .25f, Time.deltaTime), 0, 0);
+            uiManager.ChangeHealth(health);
             Mathf.Clamp(health, 0, 100);
+            //transform.localScale += new Vector3(Mathf.Lerp(transform.localScale.x, transform.localScale.x - .25f, Time.deltaTime), 0, 0);
         }
-
-        if (health <= 15)
-            renderer.material.color = Color.red;
 
         if (health <= 0)
         {
             //Destroy(gameObject); // Destroy parent which is the tank or handle whatever happens to the tank once it runs out of health	
-
         }
     }
 }
