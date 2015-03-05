466ced4c49eb83b27430fbd505ade467ef958952
skyflyer55 <jlecoq@eatel.net>
2015-03-05 13:58:10 -0600

    freddy  is ready baby

---
+++ Assets/RotateEnemyHealth.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateEnemyHealth : MonoBehaviour {
+    GameObject[] tanks;
+    public GameObject health;
+	// Use this for initialization
+	void Start () {
+        tanks = GameObject.FindGameObjectsWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    foreach(GameObject tank in tanks){
+            tank.transform.Find("TankHealthSystemCanvas").LookAt(transform);
+        }
+	}
+}
