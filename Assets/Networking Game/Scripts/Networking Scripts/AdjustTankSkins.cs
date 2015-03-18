e9358c50cc73992743fa2e21bceedbe442ecb7f6
Adam Mitchell <adamryanmitchell15@gmail.com>
2015-03-18 14:03:47 -0500

    Put my scripts in and on correct objects etc
    rotate enemy  health script, adjust tank skins script

---
+++ Assets/Networking Game/Scripts/Networking Scripts/AdjustTankSkins.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdjustTankSkins : Photon.MonoBehaviour {
+
+	public Material eagleMaterial;
+	public Material excorsistMaterial;
+	public Material wolfMaterial;
+	public Material angelMaterial;
+	private MeshRenderer[] meshes;
+	private bool teamed;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		teamed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(!teamed && photonView.isMine)
+		{
+			if(this.photonView.owner.customProperties["Team"] == "Eagles")
+			{
+				teamed = true;
+				photonView.RPC("ChangeMyTexture",PhotonTargets.All,1);
+
+			}else if(this.photonView.owner.customProperties["Team"] == "Excorsist")
+			{
+				teamed = true;
+				photonView.RPC("ChangeMyTexture",PhotonTargets.All,2);
+
+			}else if(this.photonView.owner.customProperties["Team"] == "Wolves")
+			{
+				teamed = true;
+				photonView.RPC("ChangeMyTexture",PhotonTargets.All,3);
+				
+			}else if(this.photonView.owner.customProperties["Team"] == "Angel")
+			{
+				teamed = true;
+				photonView.RPC("ChangeMyTexture",PhotonTargets.All,4);
+
+			}
+		}
+	}
+
+	[RPC]
+	void ChangeMyTexture(int team)
+	{
+		meshes = this.GetComponentsInChildren<MeshRenderer>();
+
+		if(team == 1)
+		{
+			foreach(MeshRenderer theMesh in meshes)
+			{
+				if ((theMesh.name == "MainGun Mesh"))
+				{
+					theMesh.gameObject.renderer.materials[1].CopyPropertiesFromMaterial(eagleMaterial);
+					Debug.Log("EAGLE TURRET MESH SHOULD BE SET");
+				}
+				else{
+					theMesh.gameObject.renderer.material = eagleMaterial;
+					Debug.Log("EAGLE MESH SHOULD BE SET");
+				}
+			}
+		}else if(team == 2)
+		{
+			foreach(MeshRenderer theMesh in meshes)
+			{
+				if ((theMesh.name == "MainGun Mesh"))
+				{
+					theMesh.gameObject.renderer.materials[1].CopyPropertiesFromMaterial(excorsistMaterial);
+					Debug.Log("EXCORSIST TURRET MESH SHOULD BE SET");
+				}
+				else{
+					theMesh.gameObject.renderer.material = excorsistMaterial;
+					Debug.Log("EXCORSIST MESH SHOULD BE SET");
+				}
+			}
+		}else if(team == 3)
+		{
+			foreach(MeshRenderer theMesh in meshes)
+			{
+				if ((theMesh.name == "MainGun Mesh"))
+				{
+					theMesh.gameObject.renderer.materials[1].CopyPropertiesFromMaterial(wolfMaterial);
+					Debug.Log("WOLVES TURRET MESH SHOULD BE SET");
+				}
+				else{
+					theMesh.gameObject.renderer.material = wolfMaterial;
+					Debug.Log("WOLVES MESH SHOULD BE SET");
+				}
+			}
+		}else if(team == 4)
+		{
+			foreach(MeshRenderer theMesh in meshes)
+			{
+				if ((theMesh.name == "MainGun Mesh"))
+				{
+					theMesh.gameObject.renderer.materials[1].CopyPropertiesFromMaterial(angelMaterial);
+					Debug.Log("ANGEL TURRET MESH SHOULD BE SET");
+				}
+				else{
+					theMesh.gameObject.renderer.material = angelMaterial;
+					Debug.Log("ANGEL MESH SHOULD BE SET");
+				}
+			}
+		}
+	}
+}
--- Assets/Networking Game/Scripts/Testing Scripts/RotateEnemyHealth.cs
+++ Assets/Networking Game/Scripts/Testing Scripts/RotateEnemyHealth.cs
@@ -2,16 +2,33 @@
 using System.Collections;
 
 public class RotateEnemyHealth : MonoBehaviour {
-    GameObject[] tanks;
+	
+	private GameObject[] tanks;
+	
+	private GameObject currentTankHealth;
+	//public GameObject rotateMe;
+	
 	// Use this for initialization
 	void Start () {
-        tanks = GameObject.FindGameObjectsWithTag("Player");
+		tanks = GameObject.FindGameObjectsWithTag("Player");
+		currentTankHealth = null;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    foreach(GameObject tank in tanks){
-            tank.transform.Find("TankHealthSystemCanvas").LookAt(transform);
-        }
+		foreach(GameObject tank in tanks){
+			//if(tank.name != gameObject.name)
+			//{
+			if(tank.transform.Find("TankHealthSystem").gameObject != null)
+			{
+				currentTankHealth = (GameObject)tank.transform.Find("TankHealthSystem").gameObject;
+			}
+			//currentTankHealth.transform.LookAt(transform);
+			if(currentTankHealth != null)
+			{
+				currentTankHealth.transform.LookAt(Camera.main.transform);
+			}
+			//}
+		}
 	}
 }
