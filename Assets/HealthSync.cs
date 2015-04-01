d6741457269b74e7c02f8b912d22d9ea4a75b505
jarredWynan <jaw4848@louisiana.edu>
2015-04-01 13:06:57 -0500

    unknown
    random files

---
+++ Assets/HealthSync.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthSync : Photon.MonoBehaviour {
+    public int health;
+    public UIManager uiManager;
+	// Use this for initialization
+    //void Start () {
+    //    if (photonView.isMine)
+    //    {
+    //        this.enabled = false;//Only enable inter/extrapol for remote players
+    //    }
+    //    else
+    //    {
+    //        health = (int)PhotonNetwork.player.customProperties["Health"];
+    //        uiManager = FindObjectOfType<UIManager>();
+    //    }
+    //}
+
+    //void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    //{
+    //    // Always send transform (depending on reliability of the network view)
+    //    if (stream.isWriting)
+    //    {
+    //        stream.SendNext(health);
+    //        Debug.Log(info.sender.allProperties);
+    //    }
+    //    // When receiving, buffer the information
+    //    else
+    //    {
+    //        health = (int)stream.ReceiveNext();
+    //    }
+    //}
+
+    //void Update()
+    //{
+    //    int oldHealth = (int)PhotonNetwork.player.customProperties["Health"];
+    //    if(oldHealth != health){
+    //        Debug.Log("Yeah");
+    //        uiManager.ChangeHealth(oldHealth);
+    //    }
+    //}
+    void Update()
+    {
+
+    }
+}
