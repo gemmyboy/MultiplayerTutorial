b0481a4a365d828f1db7b85f01489d0a25e7bfb2
Jacob LeCoq <jlecoq@eatel.net>
2015-04-13 01:19:25 -0500

    starting to put things together

--- Assets/Networking Game/Scripts/Networking Scripts/NetworkManager.cs
+++ Assets/Networking Game/Scripts/Networking Scripts/NetworkManager.cs
@@ -92,6 +92,7 @@ public class NetworkManager : PunBehaviour
     TankGunController tankGun;
     GameObject Target;
     MeshRenderer[] meshes;
+    GameObject newPlayerObject;
     void OnLevelWasLoaded(int level)
     {
         if (level == 1 || Start_Menu_Server_Check.SceneNameGame == "Demo Scene")
@@ -141,7 +142,18 @@ public class NetworkManager : PunBehaviour
         //--------------------------------------------------------------------------------------------------------------
 
         //Instanitate Tank
-        GameObject newPlayerObject = PhotonNetwork.Instantiate("T-90_Prefab_Network", spawnPoint, Quaternion.identity, 0);
+        if(PhotonNetwork.room.customProperties["GameType"].ToString() != "Omega Tank"){
+            if(PhotonNetwork.player.customProperties["TheOmega"].ToString() == "1"){
+                Debug.Log("spawn omega");
+                newPlayerObject = PhotonNetwork.Instantiate("T-90_Prefab_Network_Omega", spawnPoint, Quaternion.identity, 0);
+            }
+            else
+            {
+                newPlayerObject = PhotonNetwork.Instantiate("T-90_Prefab_Network", spawnPoint, Quaternion.identity, 0);
+            }
+        }else{
+            newPlayerObject = PhotonNetwork.Instantiate("T-90_Prefab_Network", spawnPoint, Quaternion.identity, 0);
+        }
         //Add the camera target
         orbit = FindObjectOfType<MouseOrbitC>();
         //add the tankgun target
@@ -250,46 +262,6 @@ public class NetworkManager : PunBehaviour
                 spawnPoint = new Vector3(400, 400, 300);
             }
         }
-    }
-
-    public GameObject sphere;
-    bool checkSpawn(Transform pos)
-    {
-        RaycastHit hit;
-        Vector3 positionCheck = new Vector3(pos.position.x,300,pos.position.z);
-        if (Physics.Raycast(positionCheck, Vector3.down, out hit, 250.0f))
-        {
-            Debug.DrawRay(positionCheck, Vector3.down * hit.distance, Color.blue, 200.0f);
-            if(hit.collider.gameObject.tag == "Terrain"){
-                Collider[] hitColliders = Physics.OverlapSphere(hit.point, 100.0f);
-                Debug.Log(hitColliders.Length);
-                if (hitColliders.Length == 0)
-                {
-                    spawnPoint = hit.point + new Vector3(0,10,0);
-                    return true;
-                }
-                else if(hitColliders.Length == 1){
-                    if(hitColliders[0].tag == "Terrain"){
-                        Debug.Log("Ready to spawn");
-                        spawnPoint = hit.point + new Vector3(0, 15, 0);
-                        return true;
-                    }
-                }
-                else
-                {
-                    foreach (Collider collider in hitColliders)
-                    {
-                        Debug.Log(collider.gameObject.name);
-                    }
-                    return false;
-                }
-            }
-            else
-            {
-                return false;
-            }
-        }
-        return false;
     }
     #endregion
 }
---
+++ Assets/SpawnLaserForOmega.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnLaserForOmega : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject laser = PhotonNetwork.Instantiate("Omega_Direction_Show", transform.position, transform.rotation, 0);
+        laser.transform.SetParent(gameObject.transform);
+	}
+}
