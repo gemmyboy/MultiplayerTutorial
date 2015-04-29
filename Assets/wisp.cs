492fab056f3c8bb252d60b6e86573e8b355d927c
skyflyer55 <jlecoq@eatel.net>
2015-04-29 16:40:23 -0500

    yeagj

---
+++ Assets/wisp.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wisp : MonoBehaviour {
+
+    public bool floatup;
+    public float yvalue;
+    public float xvalue;
+    public float zvalue;
+    void Start()
+    {
+        floatup = false;
+        //xvalue = Random.Range(1, 5);
+        yvalue = Random.Range(3, 5);
+        //zvalue = Random.Range(1, 5);
+    }
+    void LateUpdate()
+    {
+        if (floatup)
+            StartCoroutine(floatingup());
+        else if (!floatup)
+            StartCoroutine(floatingdown());
+    }
+    IEnumerator floatingup(){
+        transform.position += new Vector3(xvalue * Time.deltaTime, yvalue * Time.deltaTime, zvalue * Time.deltaTime);
+        yield return new  WaitForSeconds(3);
+        floatup = false;
+    }
+    IEnumerator floatingdown(){
+        transform.position -= new Vector3(xvalue * Time.deltaTime, yvalue * Time.deltaTime, zvalue * Time.deltaTime);
+        yield return new WaitForSeconds(3);
+        floatup = true;
+ }
+}
