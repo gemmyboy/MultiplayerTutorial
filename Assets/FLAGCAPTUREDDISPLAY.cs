f19fdf3fac768357b00ed368d859a8214a1cbb44
skyflyer55 <jlecoq@eatel.net>
2015-05-01 20:21:43 -0500

    yay

---
+++ Assets/FLAGCAPTUREDDISPLAY.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FLAGCAPTUREDDISPLAY : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        StartCoroutine("");
+	}
+
+    IEnumerator liftOff()
+    {
+        yield return new WaitForSeconds(2.0f);
+        GetComponent<ParticleSystem>();
+    }
+}
---
+++ Assets/fireworksSound.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fireworksSound : MonoBehaviour {
+    public ParticleSystem system;
+    public AudioSource source;
+    public AudioClip firecrackerSound;
+
+	// Use this for initialization
+	void Start () {
+        system = GetComponent<ParticleSystem>();
+        source = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+}
